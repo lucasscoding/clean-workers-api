ea54a191db67b1d35c5a0d2fcf543f4496e889b3
lucasscode <lucass.code@gmail.com>
2021-07-25 18:50:10 -0300

    feat: add user controller

--- src/presentetion/controllers/user-controller.ts
+++ src/presentetion/controllers/user-controller.ts
@@ -1,3 +1,19 @@
+import { UserModel } from '@/data/models'
+import { LoadUserProtocol } from '@/domain/protocols'
+import { HttpResponse, HttpResponseMessage } from '@/presentetion/models'
+
 export class UserController {
-  find(): any {}
+  private readonly userService: LoadUserProtocol
+
+  constructor(userService: LoadUserProtocol) {
+    this.userService = userService
+  }
+
+  async find(id: string): Promise<HttpResponse<UserModel | HttpResponseMessage>> {
+    const user = await this.userService.loadOneUser(id)
+    return Promise.resolve({
+      statusCode: 200,
+      body: user
+    })
+  }
 }
