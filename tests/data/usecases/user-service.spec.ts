63ebeff6fb034cb406d6ae71b1bd5f2da68fa870
lucasscode <lucass.code@gmail.com>
2021-07-31 12:59:45 -0300

    test: ensure to call find by email with the correct param

--- tests/data/usecases/user-service.spec.ts
+++ tests/data/usecases/user-service.spec.ts
@@ -3,7 +3,7 @@ import { UserService } from '@/data/usecases'
 import { DatabaseRepositoryStub } from '@/tests/data/mocks'
 
 const makeSystemUnderTest = (): any => {
-  const userMock: UserModel = {
+  const mockUser: UserModel = {
     id: '123',
     name: 'any_name',
     email: 'any_email'
@@ -11,56 +11,62 @@ const makeSystemUnderTest = (): any => {
   const databaseRepositoryStub = new DatabaseRepositoryStub()
   const mockUserService: UserService = new UserService(databaseRepositoryStub)
 
-  return { userMock, mockUserService, databaseRepositoryStub }
+  return { mockUser, mockUserService, databaseRepositoryStub }
 }
 
-const { userMock, mockUserService, databaseRepositoryStub } = makeSystemUnderTest()
+const { mockUser, mockUserService, databaseRepositoryStub } = makeSystemUnderTest()
 
 describe('UserService', () => {
   it('should load a user with the current id', async () => {
-    jest.spyOn(databaseRepositoryStub, 'findById').mockResolvedValueOnce(userMock)
-    const user: UserModel = await mockUserService.findOne(userMock.id)
+    jest.spyOn(databaseRepositoryStub, 'findById').mockResolvedValueOnce(mockUser)
+    const user: UserModel = await mockUserService.findOne(mockUser.id)
     expect(user).toBeTruthy()
-    expect(user.id).toBe(userMock.id)
+    expect(user.id).toBe(mockUser.id)
   })
 
   it('should return erro if id not informed', async () => {
     const invalidId = null
-    jest.spyOn(databaseRepositoryStub, 'findById').mockResolvedValueOnce(userMock)
+    jest.spyOn(databaseRepositoryStub, 'findById').mockResolvedValueOnce(mockUser)
     const user: UserModel = await mockUserService.findOne(invalidId)
     expect(user).toBeNull()
   })
 
   it('should save a anime with sucess', async () => {
-    jest.spyOn(databaseRepositoryStub, 'save').mockResolvedValueOnce(userMock)
-    const saveFromService = await mockUserService.save(userMock)
+    jest.spyOn(databaseRepositoryStub, 'save').mockResolvedValueOnce(mockUser)
+    const saveFromService = await mockUserService.save(mockUser)
     expect(saveFromService).toBeTruthy()
-    expect(saveFromService.name).toBe(userMock.name)
-    expect(saveFromService.email).toBe(userMock.email)
+    expect(saveFromService.name).toBe(mockUser.name)
+    expect(saveFromService.email).toBe(mockUser.email)
   })
 
   it('should return null when try save with invalid email', async () => {
-    jest.spyOn(databaseRepositoryStub, 'save').mockResolvedValueOnce(userMock)
-    const userWithOutEmail = { ...userMock, email: null }
+    jest.spyOn(databaseRepositoryStub, 'save').mockResolvedValueOnce(mockUser)
+    const userWithOutEmail = { ...mockUser, email: null }
     const invalidReturn = await mockUserService.save(userWithOutEmail)
     expect(invalidReturn).toBeNull()
   })
 
+  it('sould been call findByEmail with the correct param', async () => {
+    const spy = jest.spyOn(mockUserService, 'findByEmail')
+    await mockUserService.save(mockUser)
+    expect(spy).toHaveBeenCalledWith(mockUser.email)
+  })
+
   it('should return null when try save with invalid name', async () => {
-    jest.spyOn(databaseRepositoryStub, 'save').mockResolvedValueOnce(userMock)
-    const userWithOutName = { ...userMock, name: null }
+    jest.spyOn(databaseRepositoryStub, 'save').mockResolvedValueOnce(mockUser)
+    const userWithOutName = { ...mockUser, name: null }
     const invalidReturn = await mockUserService.save(userWithOutName)
     expect(invalidReturn).toBeNull()
   })
 
   it('sould return null if email already exist', async () => {
-    jest.spyOn(mockUserService, 'findByEmail').mockResolvedValueOnce(userMock)
-    const user = await mockUserService.save(userMock)
+    jest.spyOn(mockUserService, 'findByEmail').mockResolvedValueOnce(mockUser)
+    const user = await mockUserService.save(mockUser)
     expect(user).toBeFalsy()
   })
 
   it('should return a list of user', async () => {
-    const mockList = Array(10).fill(userMock)
+    const mockList = Array(10).fill(mockUser)
     jest.spyOn(databaseRepositoryStub, 'findAll').mockResolvedValueOnce(mockList)
     const users = await mockUserService.findAll()
     expect(users).toBeTruthy()
@@ -68,11 +74,11 @@ describe('UserService', () => {
   })
 
   it('should return a user by email', async () => {
-    jest.spyOn(databaseRepositoryStub, 'findByEmail').mockResolvedValueOnce(userMock)
-    const user = await mockUserService.findByEmail(userMock.email)
+    jest.spyOn(databaseRepositoryStub, 'findByEmail').mockResolvedValueOnce(mockUser)
+    const user = await mockUserService.findByEmail(mockUser.email)
     expect(user).toBeTruthy()
-    expect(user.name).toBe(userMock.name)
-    expect(user.email).toBe(userMock.email)
+    expect(user.name).toBe(mockUser.name)
+    expect(user.email).toBe(mockUser.email)
   })
 
   it('should return null with invalid email', async () => {
--- tests/presentetion/controllers/user-controller.spec.ts
+++ tests/presentetion/controllers/user-controller.spec.ts
@@ -3,23 +3,23 @@ import { UserController } from '@/presentetion/controllers'
 import { UserServiceStub } from '@/tests/data/mocks'
 
 const makeSystemUnderTest = (): any => {
-  const userMock: UserModel = {
+  const mockUser: UserModel = {
     id: '123',
     name: 'any_name',
     email: 'any_email'
   }
   const userServiceStub = new UserServiceStub()
   const userController: UserController = new UserController(userServiceStub)
-  return { userMock, userController, userServiceStub }
+  return { mockUser, userController, userServiceStub }
 }
 
 describe('User Controller', () => {
   it('should return 200 with correct id', async () => {
-    const { userMock, userController, userServiceStub } = makeSystemUnderTest()
-    jest.spyOn(userServiceStub, 'findOne').mockResolvedValueOnce(userMock)
-    const httpResponse = await userController.find(userMock.id)
+    const { mockUser, userController, userServiceStub } = makeSystemUnderTest()
+    jest.spyOn(userServiceStub, 'findOne').mockResolvedValueOnce(mockUser)
+    const httpResponse = await userController.find(mockUser.id)
     expect(httpResponse).toBeTruthy()
     expect(httpResponse.statusCode).toBe(200)
-    expect(httpResponse.body.id).toBe(userMock.id)
+    expect(httpResponse.body.id).toBe(mockUser.id)
   })
 })
