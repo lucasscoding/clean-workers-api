dd6a8b9158b4850ec32f681acf7cb505f9463180
Lucas Lopes <lucass.code@gmail.com>
2022-04-23 15:51:54 -0300

    refactor: add http helper on user controller

--- src/presentetion/controllers/user-controller.ts
+++ src/presentetion/controllers/user-controller.ts
@@ -1,7 +1,7 @@
 import { UserModel } from '@/data/models'
 import { LoadUserProtocol, SaveUserProtocol } from '@/domain/protocols'
 import { HttpResponse, HttpResponseMessage } from '@/presentetion/models'
-
+import { HttpHelper } from '@/presentetion/helpers'
 export class UserController {
   private readonly loadUserService: LoadUserProtocol
   private readonly saveUserService: SaveUserProtocol
@@ -13,33 +13,21 @@ export class UserController {
 
   async find(id: string): Promise<HttpResponse<UserModel | HttpResponseMessage>> {
     const user = await this.loadUserService.findOne(id)
-    return Promise.resolve({
-      statusCode: 200,
-      body: user
-    })
+    return HttpHelper.ok(user)
   }
 
   async save(user: UserModel): Promise<HttpResponse<UserModel | HttpResponseMessage>> {
     const saved = await this.saveUserService.save({ user })
-    return Promise.resolve({
-      statusCode: 201,
-      body: saved
-    })
+    return HttpHelper.created(saved)
   }
 
   async findAll(): Promise<HttpResponse<Array<UserModel> | HttpResponseMessage>> {
     const users = await this.loadUserService.findAll()
-    return Promise.resolve({
-      statusCode: 200,
-      body: users
-    })
+    return HttpHelper.ok(users)
   }
 
   async findByEmail(email: string): Promise<HttpResponse<UserModel | HttpResponseMessage>> {
     const user = await this.loadUserService.findByEmail(email)
-    return Promise.resolve({
-      statusCode: 200,
-      body: user
-    })
+    return HttpHelper.ok(user)
   }
 }
--- src/presentetion/helpers/http-helper.ts
+++ src/presentetion/helpers/http-helper.ts
@@ -1,17 +1,21 @@
 import { HttpResponse } from '@/presentetion/models'
-
 export class HttpHelper {
+  public static readonly OK: number = 200
+  public static readonly CREATED: number = 201
+  public static readonly BAD_REQUEST: number = 400
+
   static badRequest(error: Error): Promise<HttpResponse> {
-    return Promise.resolve({
-      statusCode: 400,
-      body: error
-    })
+    const result = { body: error, statusCode: HttpHelper.BAD_REQUEST }
+    return Promise.resolve(result)
   }
 
-  static ok<T>(body: T): Promise<HttpResponse> {
-    return Promise.resolve({
-      statusCode: 200,
-      body: body
-    })
+  static ok<T>(body: T): Promise<HttpResponse<T>> {
+    const result = { body: body, statusCode: HttpHelper.OK }
+    return Promise.resolve(result)
+  }
+
+  static created<T>(body: T): Promise<HttpResponse<T>> {
+    const result = { body: body, statusCode: HttpHelper.CREATED }
+    return Promise.resolve(result)
   }
 }
