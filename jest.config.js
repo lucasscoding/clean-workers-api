993b75490e1767a69b6c493b12b43d2e34fe9a3a
Lucas Lopes <lucass.code@gmail.com>
2022-04-26 18:09:00 -0300

    chore: remove index files from jest test

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   roots: ['<rootDir>/tests'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/main/**'
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/index.ts'
   ],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
